1bb3c1e567d2ac6eaf24c26f78e13bfae475b20c
rdtsc <rdtsc@users.noreply.github.com>
2020-03-20 22:55:26 -0400

    Add a solution to challenge 4
    The challenge's backend processing script appears to be broken.
    It always seems to return an invalid HTTP referer error, even though
    an apparently valid one is being provided in the POST header.

---
+++ src/04/readme.md
@@ -0,0 +1,20 @@
+Challenge 4
+-----------
+
+Challenge Endpoint: `http://challenges.enigmagroup.org/programming/4/`
+
+> The goal is to take something from this image and make it into a
+> readable/plain text answer.
+>
+> Example:
+>
+> ![Example](./data/example.png)
+>
+> Steganography skills may be of help here, but they're not required.
+>
+> Note: Send your answer to "image.php".
+> If you're completely lost, try thinking about the colors used.
+>
+> The time limit for this mission is 1 second.
+
+The image is non-static and refreshes with each request.
---
+++ src/04/solve.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const {PNG}  = require('pngjs'),
+      chunk  = require('lodash.chunk'),
+      assert = require('assert'),
+      enigma = require('~/lib/enigma');
+
+function decodeImage(data)
+{
+  const img = PNG.sync.read(data);
+
+  const area = img.width * img.height;
+
+  assert(((area >>> 3) << 3) == area);
+
+  const payload = [];
+
+  for(let y = 0; y < img.height; ++y)
+  for(let x = 0; x < img.width;  ++x)
+  {
+    const i = (img.width * y + x) << 2;
+
+    payload.push(+!img.data[i]);
+  }
+
+  return chunk(payload, 8).map(bin => parseInt(bin.join(''), 2))
+                          .map(int => String.fromCharCode(int))
+                          .join('');
+}
+
+(async (problemId = '4/image.php') =>
+{
+  const {data: img} = await enigma.get(problemId, 'arraybuffer');
+
+  const [preamble, answer] = decodeImage(img).split(':');
+
+  const payload = {answer: answer};
+
+  console.log(await enigma.post(problemId, payload));
+})();
